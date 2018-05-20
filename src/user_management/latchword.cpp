5f8950d9950e871cb99c55dbd5d8ab837280039f
Farid Arbai <35288820+FaridArbai@users.noreply.github.com>
2018-05-20 23:53:40 +0200

    Adding new class to simplify PTP keys management

--- src/user_management/latchword.cpp
+++ src/user_management/latchword.cpp
@@ -1,6 +1,47 @@
 #include "latchword.h"
 
-Latchword::Latchword()
-{
+Latchword::Latchword(string password){
+    int password_len = password.length();
+    int key_len = SymmetricEngine::KEY_LENGTH;
+    string key;
 
+    if (password_len>key_len){
+        key = password.substr(0,key_len);
+    }
+    else{
+        int pad_len = key_len - password_len;
+
+        for(int i=0; i<pad_len; i++){
+            key += SymmetricEngine::PAD_TOKEN;
+        }
+    }
+
+    this->setPTPKey(key);
+    this->ptp_engine.init(key,key,key,key);
+}
+
+string Latchword::getPTPKey() const{
+   return this->ptp_key;
+}
+
+void Latchword::setPTPKey(string ptp_key){
+    this->ptp_key = ptp_key;
+}
+
+string Latchword::encrypt(string message){
+    string encr = this->ptp_engine.encrypt(message);
+    this->ptp_engine.refresh();
+
+    return encr;
+}
+
+string Latchword::decrypt(string encr){
+    string decr = this->ptp_engine.decrypt(encr);
+    this->ptp_engine,refresh();
+
+    return decr;
+}
+
+string Latchword::toString(){
+    return this->getPTPKey();
 }
--- src/user_management/latchword.h
+++ src/user_management/latchword.h
@@ -1,11 +1,26 @@
 #ifndef LATCHWORD_H
 #define LATCHWORD_H
 
+#include <string>
+#include <src/encryption_engines/symmetricengine.h>
 
-class Latchword
-{
+using namespace std;
+
+
+class Latchword{
 public:
-    Latchword();
+    Latchword(string password);
+
+    string getPTPKey() const;
+    void setPTPKey(string ptp_key);
+
+    string encrypt(string message);
+    string decrypt(string encr);
+
+    string toString();
+private:
+    string ptp_key;
+    SymmetricEngine ptp_engine;
 };
 
 #endif // LATCHWORD_H
