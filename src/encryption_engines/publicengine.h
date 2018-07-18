5130762c3f7752c58983ff7a94151d255909bc4c
Farid Arbai <35288820+FaridArbai@users.noreply.github.com>
2018-07-18 23:24:11 +0200

    Implented full native RSA support on Android

--- src/encryption_engines/publicengine.h
+++ src/encryption_engines/publicengine.h
@@ -3,6 +3,15 @@
 
 #include "src/protocol_messages/encoding/base64.h"
 
+#ifndef ANDROID
+#include <openssl/rsa.h>
+#include <openssl/pem.h>
+#include <openssl/bio.h>
+#endif
+
+#include <openssl/sha.h>
+
+
 #include <string.h>
 #include <string>
 
@@ -12,10 +21,16 @@ class PublicEngine{
 public:
     PublicEngine() = delete;
 
+    static string encrypt(string message);
     static bool verifySignature(const string& payload, const string& signature);
 
+    static string sha256(string payload);
+    static string computeBase64Hash(const string& payload);
 private:
     static const char PUBLIC_KEY[];
+
+    static string decryptHash(const string& signature);
+    static void logchars(string name, string s);
 };
 
 #endif // PUBLICENGINE_H
