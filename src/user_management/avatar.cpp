3faa998932c423c49e25039914014649a78c64cc
Farid Arbai <35288820+FaridArbai@users.noreply.github.com>
2018-07-23 23:50:56 +0200

    Implemented a one-time compute scheme for primary color

--- src/user_management/avatar.cpp
+++ src/user_management/avatar.cpp
@@ -1,16 +1,25 @@
 #include "avatar.h"
 
+const int Avatar::BRIGHTNESS_THRESHOLD = 255/2;
+
 Avatar::Avatar(){
 }
 
 Avatar::Avatar(string image_path){
     this->setImagePath(image_path);
+    this->computeThemeColor();
 }
 
 Avatar::Avatar(string username, string format, string image_bin){
     string image_path = IOManager::getImagePath(username,format);
-    this->setImagePath(image_path);
     IOManager::saveImage(image_path,image_bin);
+    this->setImagePath(image_path);
+
+}
+
+Avatar::Avatar(const Avatar& orig){
+    this->setImagePath(orig.getImagePath());
+    this->setColor(orig.getColor());
 }
 
 string Avatar::toString(){
@@ -26,8 +35,21 @@ string Avatar::getImagePath() const{
     return this->image_path;
 }
 
+int Avatar::getColor() const{
+    return this->color;
+}
+
 void Avatar::setImagePath(string image_path){
     this->image_path = image_path;
+}
+
+void Avatar::computeThemeColor(){
+    int color = Avatar::extractMedianColor(this->image_path);
+    this->setColor(color);
+}
+
+void Avatar::setColor(int color){
+    this->color = color;
 }
 
 string Avatar::getImageFormat(string image_path){
@@ -84,9 +106,64 @@ string Avatar::getBinary(){
     return image_bin;
 }
 
+int Avatar::extractMedianColor(string source){
+    qDebug() << "************** EXTRACT IS CALLED ***************" << endl;
+    QImage image(QString::fromStdString(source));
+    int height;
+    int width;
+    QColor color;
+    vector<int> v_red;
+    vector<int> v_green;
+    vector<int> v_blue;
+    int red;
+    int green;
+    int blue;
+    int final_color;
 
+    if(image.isNull()==false){
+        height = (image.height()>64)?(64):(image.height());
+        width = image.width();
 
+        for(int y=0; y<height; y++){
+            for(int x=0; x<width; x++){
+                color = QColor(image.pixel(y,x));
+                v_red.push_back(color.red());
+                v_green.push_back(color.green());
+                v_blue.push_back(color.blue());
+            }
+        }
 
+        red = calculateMedian(v_red);
+        green = calculateMedian(v_green);
+        blue = calculateMedian(v_blue);
+
+        QColor rgb = QColor::fromRgb(red,green,blue);
+        int brightness_th = 255/2;
+        int brightness_level = rgb.value();
+
+        if(brightness_level>brightness_th){
+            rgb = QColor::fromHsv(rgb.hue(),rgb.saturation(),brightness_th).toRgb();
+        }
+
+        final_color = (rgb.red()<<16) + (rgb.green()<<8) + (rgb.blue()) + 0xFF000000;
+    }
+    else{
+        final_color = 0xFFC0C0C0;
+    }
+
+    return final_color;
+}
+
+int Avatar::calculateMedian(vector<int>& v){
+    int median;
+    int size = v.size();
+
+    std::sort(v.begin(),v.end());
+
+    median = v[size/2];
+
+    return median;
+}
 
 
 
--- src/user_management/avatar.h
+++ src/user_management/avatar.h
@@ -3,6 +3,12 @@
 
 #include <string>
 #include "src/user_management/iomanager.h"
+#include <QImage>
+#include <QColor>
+#include <QString>
+#include <QDebug>
+
+
 
 using std::string;
 
@@ -11,19 +17,31 @@ public:
     Avatar();
     Avatar(string image_path);
     Avatar(string username, string format, string image_bin);
+    Avatar(const Avatar& orig);
 
     string toString();
 
     string getImagePath() const;
+    int getColor() const;
+
     void setImagePath(string image_path);
+    void setColor(int color);
 
     string getFormat();
     string getBinary();
 
+    void computeThemeColor();
+
     static string getImageFormat(string image_path);
 
 private:
+    static const int BRIGHTNESS_THRESHOLD;
+
     string image_path;
+    int color;
+
+    static int extractMedianColor(string source);
+    static int calculateMedian(std::vector<int>& v);
 };
 
 #endif // AVATAR_H
