0020d2fdc377df52185cb3db614e1008e8d33a82
Farid Arbai <35288820+FaridArbai@users.noreply.github.com>
2018-08-22 23:50:51 +0200

    Updated message-box color

--- qml/Constants.js
+++ qml/Constants.js
@@ -1,11 +1,11 @@
 .pragma library
 
 var STD_ELEVATION           =   5;
-var TOOLBAR_COLOR           =   "#016479";
-var STATUSBAR_COLOR         =   "#014d60";
+var TOOLBAR_COLOR           =   "#0084BF";
+var STATUSBAR_COLOR         =   "#006896";
 var TOOLBAR_COLOR_2         =   "#0294bd";
 var GRADIENT_TOOLBAR_COLOR  =   "#017379";
-var VIBRANT_COLOR           =   "#0097A7";
+var VIBRANT_COLOR           =   "#0084BF";
 
 var TOP_LOGIN_COLOR         =   "#1f5a7a";
 var BOTTOM_LOGIN_COLOR      =   "#0f808a";
@@ -33,14 +33,13 @@ var USERNAME_REGEX          =   /^[0-9a-zA-Z]+$/;
 var TOOLBUTTON_SIZE         =   40;
 var SENDBUTTON_SIZE         =   80;
 
-var LOGIN_STATUSBAR_COLOR               =   0X00000000;
-var CONTACTS_STATUSBAR_COLOR            =   0xff014d60;
+var LOGIN_STATUSBAR_COLOR               =   0xFF0084BF;
+var CONTACTS_STATUSBAR_COLOR            =   0xFF006896;
 var IMAGEPROCESSING_STATUSBAR_COLOR     =   0xFF000000;
 
 var SIDE_FACTOR         =   (157/2880);
 var SPACING_FACTOR      =   (7/180);
 var ICON_FACTOR         =   (34/720);
-
 
 
 var MENU_COLOR          =   "#F3F3F3";
@@ -52,6 +51,7 @@ var MENU_RADIUS_FACTOR  =   1/32;
 var MENUITEM_PIXEL_FACTOR   =   28;
 var MENUITEM_HEIGHT_FACTOR  =   3;
 var MENUITEM_PRESSED_COLOR  =   "#10888888";
+var MENU_HREF               =   1135;
 
 var MENU_BORDERMARGIN_FACTOR    =   1/32;
 var MENU_VERTICALPAD_FACTOR     =   1/2;
@@ -69,8 +69,16 @@ var IMAGEPROCESSING_CANVAS_COLOR = "#40FFFFFF";
 
 var DROPSHADOW_COLOR    =   "#42000000";
 
+var LogPage = {
+    TEXT_COLOR      :   "#AFFFFFFF",
+    FOCUS_COLOR     :   "#CFFFFFFF",
+    HINT_COLOR      :   "#7FC1DF",
+    SEPARATOR_COLOR :   "#7FFFFFFF",
+}
+
+
 var ContactPage = {
-    CONTACTNAME_COLOR       : "#016479",
+    CONTACTNAME_COLOR       : "#0084BF",
     PRESENCE_COLOR          : "#626665",
     LASTMESSAGE_COLOR       : "#626665",
     UNREAD_CONTAINER_COLOR  : "#029cbd",
@@ -93,10 +101,10 @@ var WREF = 720;
 var HREF = 1080;
 
 var ConversationPage = {
-    PERSONAL_MESSAGE_BACKGROUND :   "#E1FFC7",
+    PERSONAL_MESSAGE_BACKGROUND :   "#e1f5fe",//"#D4EAF4",//"#C9E7FF",
     CONTACT_MESSAGE_BACKGROUND  :   "#FFFFFF",
     ERROR_MESSAGE_BACKGROUND    :   "#E53935",
-    NEW_DAY_BACKGROUND          :   "#D4EAF4",
+    NEW_DAY_BACKGROUND          :   "#C9E7FF",
     SENDBUTTON_COLOR            :   "#0097A7",
     ERROR_MESSAGE               :   "This message couldn't be decrypted, please change this contact's latchkey.",
     SEND_FIELD_MARGIN           :   9/HREF,
@@ -125,17 +133,33 @@ var TextInput = {
 }
 
 
+var Button = {
+    DARK_ANIMATION_COLOR    :   "#C0C0C0",
+    LIGHT_ANIMATION_COLOR   :   "#FFFFFF",
+    ANIMATION_DURATION      :   250,
+}
 
 
+function addTransparency(transparency,color){
+    var has_one_character = transparency<=0x0F;
+    var transparency_str = (has_one_character?("0"):("")) + transparency.toString(16);
+    var final_color = "#" + transparency_str + color.substr(1);
 
+    return final_color;
+}
 
+function addFloatTransparency(transparency_float,color){
+    var transparency = Math.round(transparency_float*255);
+    var has_one_character = transparency<=0x0F;
+    var transparency_str = (has_one_character?("0"):("")) + transparency.toString(16);
+    var final_color = "#" + transparency_str + color.substr(1);
 
+    return final_color;
+}
 
-
-
-
-
-
+var SHORT_DURATION      =   100;
+var GENERAL_DURATION    =   250;
+var VISIBLE_DURATION    =   550;
 
 
 
